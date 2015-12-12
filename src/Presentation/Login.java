5f58dc01a2307ac1d6356a8ddd00daf4bc8795c4
JoaoFCosta <dinojoaocosta@gmail.com>
2015-12-12 17:34:34 +0000

    Revert to 038bd26bbc03d8ab52e45828e08bd87615b33933

--- src/Presentation/Login.java
+++ src/Presentation/Login.java
@@ -82,11 +82,6 @@ public class Login extends javax.swing.JFrame {
         );
 
         jButton1.setText("Login");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -183,11 +178,6 @@ public class Login extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        this.dispose();
-        new VotoLegislativas().setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
