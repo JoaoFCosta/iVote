5323ad43cca013ca07c1c52fc0b03241064d21c2
Martinho Aragão <martinhoaragao@gmail.com>
2015-12-21 12:08:08 +0000

    Fix iVote DB Creation wrong foreign key

--- iVoteCriacao.sql
+++ iVoteCriacao.sql
@@ -5,28 +5,28 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema iVote
+-- Schema iVoteDB
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema iVote
+-- Schema iVoteDB
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `iVote` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `iVote` ;
+CREATE SCHEMA IF NOT EXISTS `iVoteDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `iVoteDB` ;
 
 -- -----------------------------------------------------
--- Table `iVote`.`Eleicao`
+-- Table `iVoteDB`.`Eleicao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Eleicao` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Eleicao` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`rondaPresidencial`
+-- Table `iVoteDB`.`RondaPresidencial`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`rondaPresidencial` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`RondaPresidencial` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `idEleicao` INT NOT NULL COMMENT '',
   `ronda` INT NOT NULL COMMENT '',
@@ -35,16 +35,16 @@ CREATE TABLE IF NOT EXISTS `iVote`.`rondaPresidencial` (
   PRIMARY KEY (`id`)  COMMENT '',
   CONSTRAINT `fk_Presidencial_Eleicao1`
     FOREIGN KEY (`idEleicao`)
-    REFERENCES `iVote`.`Eleicao` (`id`)
+    REFERENCES `iVoteDB`.`Eleicao` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Legislativa`
+-- Table `iVoteDB`.`Legislativa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Legislativa` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Legislativa` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `idEleicao` INT NOT NULL COMMENT '',
   `data` DATETIME NOT NULL COMMENT '',
@@ -52,16 +52,16 @@ CREATE TABLE IF NOT EXISTS `iVote`.`Legislativa` (
   INDEX `fk_Legislativa_Eleicao1_idx` (`idEleicao` ASC)  COMMENT '',
   CONSTRAINT `fk_Legislativa_Eleicao1`
     FOREIGN KEY (`idEleicao`)
-    REFERENCES `iVote`.`Eleicao` (`id`)
+    REFERENCES `iVoteDB`.`Eleicao` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Circulo`
+-- Table `iVoteDB`.`Circulo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Circulo` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Circulo` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `distrito` VARCHAR(45) NOT NULL COMMENT '',
   `idLegislativa` INT NOT NULL COMMENT '',
@@ -69,40 +69,40 @@ CREATE TABLE IF NOT EXISTS `iVote`.`Circulo` (
   INDEX `fk_Circulo_Legislativa1_idx` (`idLegislativa` ASC)  COMMENT '',
   CONSTRAINT `fk_Circulo_Legislativa1`
     FOREIGN KEY (`idLegislativa`)
-    REFERENCES `iVote`.`Legislativa` (`id`)
+    REFERENCES `iVoteDB`.`Legislativa` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`AssembleiaVoto`
+-- Table `iVoteDB`.`AssembleiaVoto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`AssembleiaVoto` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`AssembleiaVoto` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `nome` VARCHAR(45) NOT NULL COMMENT '',
   `idCirculo` INT NULL COMMENT '',
-  `idPresidencial` INT NULL COMMENT '',
+  `idRondaPresidencial` INT NOT NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '',
   INDEX `fk_Freguesia_Circulo1_idx` (`idCirculo` ASC)  COMMENT '',
-  INDEX `fk_Freguesia_RondaPresencial1_idx` (`idPresidencial` ASC)  COMMENT '',
+  INDEX `fk_AssembleiaVoto_rondaPresidencial1_idx` (`idRondaPresidencial` ASC)  COMMENT '',
   CONSTRAINT `fk_Freguesia_Circulo1`
     FOREIGN KEY (`idCirculo`)
-    REFERENCES `iVote`.`Circulo` (`id`)
+    REFERENCES `iVoteDB`.`Circulo` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Freguesia_RondaPresencial1`
-    FOREIGN KEY (`idPresidencial`)
-    REFERENCES `iVote`.`rondaPresidencial` (`ronda`)
+  CONSTRAINT `fk_AssembleiaVoto_rondaPresidencial1`
+    FOREIGN KEY (`idRondaPresidencial`)
+    REFERENCES `iVoteDB`.`RondaPresidencial` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Cidadao`
+-- Table `iVoteDB`.`Cidadao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Cidadao` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Cidadao` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `idEleitor` INT NOT NULL COMMENT '',
   `password` VARCHAR(45) NOT NULL COMMENT '',
@@ -111,9 +111,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Eleitor`
+-- Table `iVoteDB`.`Eleitor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Eleitor` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Eleitor` (
   `idCidadao` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `idAssembleiaVoto` INT NOT NULL COMMENT '',
   `votou` INT NULL COMMENT '',
@@ -122,30 +122,30 @@ CREATE TABLE IF NOT EXISTS `iVote`.`Eleitor` (
   PRIMARY KEY (`idCidadao`)  COMMENT '',
   CONSTRAINT `fk_Eleitor_Freguesia`
     FOREIGN KEY (`idAssembleiaVoto`)
-    REFERENCES `iVote`.`AssembleiaVoto` (`id`)
+    REFERENCES `iVoteDB`.`AssembleiaVoto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Eleitor_Cidadao1`
     FOREIGN KEY (`idCidadao`)
-    REFERENCES `iVote`.`Cidadao` (`id`)
+    REFERENCES `iVoteDB`.`Cidadao` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Lista`
+-- Table `iVoteDB`.`Lista`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Lista` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Lista` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`AssembleiaLista`
+-- Table `iVoteDB`.`AssembleiaLista`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`AssembleiaLista` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`AssembleiaLista` (
   `idLista` INT NOT NULL COMMENT '',
   `idAssembleiaVoto` INT NOT NULL COMMENT '',
   `votos` INT NULL DEFAULT 0 COMMENT '',
@@ -154,21 +154,21 @@ CREATE TABLE IF NOT EXISTS `iVote`.`AssembleiaLista` (
   INDEX `fk_Lista_has_Freguesia_Lista1_idx` (`idLista` ASC)  COMMENT '',
   CONSTRAINT `fk_Lista_has_Freguesia_Lista1`
     FOREIGN KEY (`idLista`)
-    REFERENCES `iVote`.`Lista` (`id`)
+    REFERENCES `iVoteDB`.`Lista` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Lista_has_Freguesia_Freguesia1`
     FOREIGN KEY (`idAssembleiaVoto`)
-    REFERENCES `iVote`.`AssembleiaVoto` (`id`)
+    REFERENCES `iVoteDB`.`AssembleiaVoto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Candidato`
+-- Table `iVoteDB`.`Candidato`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Candidato` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Candidato` (
   `idCidadao` INT NOT NULL COMMENT '',
   `idLista` INT NOT NULL COMMENT '',
   PRIMARY KEY (`idCidadao`)  COMMENT '',
@@ -176,31 +176,31 @@ CREATE TABLE IF NOT EXISTS `iVote`.`Candidato` (
   INDEX `fk_Candidato_Lista1_idx` (`idLista` ASC)  COMMENT '',
   CONSTRAINT `fk_Candidato_Cidadao1`
     FOREIGN KEY (`idCidadao`)
-    REFERENCES `iVote`.`Cidadao` (`id`)
+    REFERENCES `iVoteDB`.`Cidadao` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Candidato_Lista1`
     FOREIGN KEY (`idLista`)
-    REFERENCES `iVote`.`Lista` (`id`)
+    REFERENCES `iVoteDB`.`Lista` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`Admin`
+-- Table `iVoteDB`.`Admin`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`Admin` (
-  `idAdmin` INT NOT NULL COMMENT '',
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`Admin` (
+  `id` VARCHAR(15) NOT NULL COMMENT '',
   `password` VARCHAR(45) NOT NULL COMMENT '',
-  PRIMARY KEY (`idAdmin`)  COMMENT '')
+  PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `iVote`.`AssembleiaCandidato`
+-- Table `iVoteDB`.`AssembleiaCandidato`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `iVote`.`AssembleiaCandidato` (
+CREATE TABLE IF NOT EXISTS `iVoteDB`.`AssembleiaCandidato` (
   `idAssembleiaVoto` INT NOT NULL COMMENT '',
   `idCidadao` INT NOT NULL COMMENT '',
   `votos` INT NULL COMMENT '',
@@ -209,12 +209,12 @@ CREATE TABLE IF NOT EXISTS `iVote`.`AssembleiaCandidato` (
   INDEX `fk_AssembleiaVoto_has_Candidato_AssembleiaVoto1_idx` (`idAssembleiaVoto` ASC)  COMMENT '',
   CONSTRAINT `fk_AssembleiaVoto_has_Candidato_AssembleiaVoto1`
     FOREIGN KEY (`idAssembleiaVoto`)
-    REFERENCES `iVote`.`AssembleiaVoto` (`id`)
+    REFERENCES `iVoteDB`.`AssembleiaVoto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_AssembleiaVoto_has_Candidato_Candidato1`
     FOREIGN KEY (`idCidadao`)
-    REFERENCES `iVote`.`Candidato` (`idCidadao`)
+    REFERENCES `iVoteDB`.`Candidato` (`idCidadao`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
