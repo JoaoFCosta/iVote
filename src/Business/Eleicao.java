de93c3305271530c9959238f8d3dc9f72f6c4dde
JoaoFCosta <dinojoaocosta@gmail.com>
2015-12-23 22:57:28 +0000

    Adiciona Eleicao e merge master

---
+++ src/Business/Eleicao.java
@@ -0,0 +1,23 @@
+package Business;
+import java.util.Calendar;
+
+/**
+ * @author joaocosta
+ */
+public class Eleicao {
+  private Calendar data;
+
+  /** Construtor parametrizado.
+   *  @param data Data da Eleição. */
+  public Eleicao (Calendar data) {
+    this.data = (Calendar) data.clone();
+  }
+
+  // GETTERS & SETTERS
+
+  public Calendar getData () { return (Calendar) data.clone(); }
+
+  public void setData (Calendar data) {
+    this.data = (Calendar) data.clone();
+  }
+}
