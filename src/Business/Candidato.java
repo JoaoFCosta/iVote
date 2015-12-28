fea3fbbcaa1dc6aa61b5161d90bf3850b41ba9dc
Gstavo <gustavocg@live.com.pt>
2015-12-28 02:00:38 +0000

    Removed distrito freguesia datanasc de eleitor e candidato, added candidatoDAO

--- src/Business/Candidato.java
+++ src/Business/Candidato.java
@@ -13,48 +13,9 @@ import java.util.GregorianCalendar;
  * @author joaocosta
  */
 public class Candidato extends Eleitor {
-    private String partido;
 
-    /** Construtor parametrizado.
-     * @param nome          Nome do candidato.
-     * @param codigo        Código do cartão de cidadão.
-     * @param distrito      Distrito de residência.
-     * @param freguesia     Freguesia de residência.
-     * @param nascimento    Data de nascimento.
-     * @param partido       Partido do candidato.
-     */
-    public Candidato(String nome, String codigo, String distrito, 
-            String freguesia, Calendar nascimento, String partido) {
-        super(nome, codigo, distrito, freguesia, nascimento);
-        this.partido = partido;
+    public Candidato(String nome, String codigo) {
+        super(nome, codigo);
     }
-    
-    /** @return Idade do candidato.
-     */
-    public int idade () {
-        Calendar now        = new GregorianCalendar();
-        Calendar nascimento  = super.dataNascimento();
-        int idade   = now.YEAR - nascimento.YEAR;
-        
-        if (nascimento.MONTH > now.MONTH)
-            idade--;
-        else if (nascimento.DAY_OF_MONTH > now.DAY_OF_MONTH)
-            idade--;
-        
-        return idade;
-    }
-    
-    // GETTERS & SETTERS
-    
-    /** Partido do candidato. */
-    public String partido () { return this.partido; }
-    
-    /** Alterar partido do candidato.
-     *  @param partido Novo partido do candidato. */
-    public void setPartido (String partido) { this.partido = partido; }
-    
-    /** @return Nome do candidato. */
-    public String getNome () {
-        return super.nome();
-    }
+   
 }
---
+++ src/Data/CandidatoDAO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import Business.Candidato;
+import Business.Eleicao;
+import Business.EleicaoLegislativa;
+import Business.EleicaoPresidencial;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class CandidatoDAO {
+    
+    /*
+    *   Devolve os candidatos da eleiçao presidencial id
+    */
+    
+    public List<Candidato> getCandidatosP(int id){
+    
+    Connection con  = null;
+    List lista      = (List) new ArrayList<Candidato>();
+    
+    try {
+      con                     = Connect.connect();
+
+      PreparedStatement candidatos  = con.prepareStatement(
+          "select distinct C.id, C.nome from Eleicao as E" +
+            "	inner join RondaPresidencial as RP" +
+            "	on E.id=RP.id" +
+            "		inner join AssembleiaVoto as AV" +
+                    "        on AV.idRondaPresidencial=RP.id" +
+        "			inner join AssembleiaCandidato as AC" +
+                        "            on AC.idAssembleiaVoto=AV.id" +
+        "				inner join Candidato as CA" +
+                        "                on CA.idCidadao=AC.idCidadao" +
+                                            " inner join Cidadao as C" +
+						"	on CA.idCidadao = C.id" +
+"                where E.id=" + id +";");
+      
+      
+      ResultSet rs = candidatos.executeQuery();
+
+      while (rs.next()) {       
+        String idCidadao = rs.getInt("id") + "";
+        String nome = rs.getString("nome");
+        Candidato c = new Candidato(nome,idCidadao);
+        lista.add(c);
+      }
+      
+    } catch (SQLException | ClassNotFoundException e) {
+      System.out.println(e);
+    } finally {
+      try { con.close(); }
+      catch (Exception e) { System.out.println(e); }
+    }
+
+    return lista;
+    }
+}
